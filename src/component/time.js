e7f111eb23393cb8f558160cdc58f2cfa941def5
JojoSala <joelsalacode23gmail.com>
2024-04-08 21:09:11 +0200

    manage commit

--- src/component/time.js
+++ src/component/time.js
@@ -1,17 +1,9 @@
 
 function Time() {
 
-    const date = new Date().getHours();
-    if(date < 8 && date > 18) {
-        return 'The registration is closed'
-    } else if(date >= 8 && date <= 18) {
-        return 'The registration is opened'
-    } else {
-        return 'ayayayayaya'
-    }
-    return   <h2>{date}</h2>;
+const date = new Date().getHours();
 
-    
+return <h2>{(date < 8 || date > 18) ? 'Registration is closed!' : 'Registration is Opened'}</h2>
 }
 
 export default Time;

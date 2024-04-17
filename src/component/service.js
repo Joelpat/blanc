2794d0f4fe9d7471d252f574d0682963599bb5c7
JojoSala <joelsalacode23gmail.com>
2024-04-17 15:13:38 +0200

    vraiment commit

--- src/component/service.js
+++ src/component/service.js
@@ -3,6 +3,10 @@ const Service = () => {
     const [list, setList] = useState([]);
     const [value, setValue] = useState('');
 
+    const deletItem = (index) => {
+        let temp = list.filter((item, i) => i !== index);
+        setList(temp);
+    };
     const addToList = () => {
         let tempArr = list;
         tempArr.push(value);
@@ -16,7 +20,7 @@ const Service = () => {
     <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
     <button onClick={addToList}>Add student</button>
     <ol>{list.length > 0 &&
-    list.map((item) => <li>{item}</li>)}
+    list.map((item, i) => <li>{item} <span><button onClick={() => deletItem(i)}>X</button></span></li>)}
     </ol>
     </>
     );
